8749ba87f89d45bc1abb376b13aa26febf229971
Ojo Joel Ademola <o.joeade@gmail.com>
2026-04-21 14:44:45 +0100

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-// src/App.jsx
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './pages/Hero'
 import About from './pages/About'
 import Loading from './components/Loading';
 
-
 const App = () => {
+  const [showLoading, setShowLoading] = useState(true);
+
+  if (showLoading) {
+    return <Loading onFinish={() => setShowLoading(false)} />;
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path="/"      element={<Hero />}  />
         <Route path="/about" element={<About />} />
-        <Route path="/loading" element={<Loading />} />
       </Routes>
     </Router>
   )
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import loading_pic from '../assets/loading_pic.svg';
+import background from '../assets/background.svg';
 
 
 const Loading = ({ onFinish }) => {
